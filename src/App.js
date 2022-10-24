a518cee1236a1289aa1605853c551039ceb601c8
Svitlana Orzul <111054001+OleksandrOrz@users.noreply.github.com>
2022-10-24 15:30:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <div className="App">
+    <div className="container">
+      <form><div className="row">
+        <div className="col-6"> <input type="search"></input> </div>
+        <div className="col-6"> <input type="submit"className="btn btn-primary"></input> </div>
+      </div>
+      </form>
+    </div>
+    </div> 
+  );
+}
+
+export default App;
