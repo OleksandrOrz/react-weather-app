fd193cd41e56f657cb3a03414a49e993ee2b90b8
Svitlana Orzul <111054001+OleksandrOrz@users.noreply.github.com>
2022-10-27 14:53:20 +0300

    Added corect icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(){
             wind: response.data.wind.speed,
             description:response.data.weather[0].description,
             humidity: response.data.main.humidity,
-            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain.png",
+            iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             date:new Date (response.data.dt*1000)
         })
     }
