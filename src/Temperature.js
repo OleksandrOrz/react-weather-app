6fc2ac97073edf24ce03ad76f8a841fe1721e658
Svitlana Orzul <111054001+OleksandrOrz@users.noreply.github.com>
2022-10-28 19:51:59 +0300

    Update Temperature.js

--- src/Temperature.js
+++ src/Temperature.js
@@ -17,7 +17,7 @@ setUnit("cel")
     return(
    <span>
     <span className="temperature"> {Math.round(props.temp)}</span> 
-    <span className="units">°C| <a href="#" onClick={showFahr}>°F</a> </span>
+    <span className="units">°C| <a href="/#" onClick={showFahr}>°F</a> </span>
     </span>
     )}
     else{
@@ -25,7 +25,7 @@ setUnit("cel")
         return(
                      <span>
     <span className="temperature"> {fahr}</span> 
-    <span className="units"><a href="#" onClick={showCel}>°C</a>| °F </span>
+    <span className="units"><a href="/#" onClick={showCel}>°C</a>| °F </span>
     </span>
     )
 
